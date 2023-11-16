ef1b7e7892b853d5145779548a6efa65232f8ad1
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-11-16 17:31:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ function got_poses(results){
     if(results.length>0){
         console.log(results);
         score_pulso_esq=results[0].pose.keypoints[9].score;
+        score_pulso_dir=results[0].pose.keypoints[10].score;
         
 
         pulso_esqX=results[0].pose.leftWrist.x;
@@ -57,8 +58,28 @@ function draw(){
     image(video, 0, 0, 600, 500);
     fill("red");
     stroke("red");
+    if(score_pulso_dir>0.2){
+        circle(pulso_dirX, pulso_dirY, 20);
+        
+        if(pulso_dirY>0 && pulso_dirY<=100){
+            document.getElementById("speed").innerHTML="velocidade = 0.5x";
+            musica.rate(0.5);
+        } else if(pulso_dirY>100 && pulso_dirY<=200){
+            document.getElementById("speed").innerHTML="velocidade = 1x";
+            musica.rate(1);
+        } else if(pulso_dirY>200 && pulso_dirY<=300){
+            document.getElementById("speed").innerHTML="velocidade = 1.5x";
+            musica.rate(1.5);
+        } else if(pulso_dirY>300 && pulso_dirY<=400){
+            document.getElementById("speed").innerHTML="velocidade = 2x";
+            musica.rate(2);
+        } else if(pulso_dirY>400){
+            document.getElementById("speed").innerHTML="velocidade = 2.5x";
+            musica.rate(2.5);
+        }
+    }
     
-    //if(score_pulso_esq>0.2){
+    if(score_pulso_esq>0.2){
         console.log("entrou");
         circle(pulso_esqX, pulso_esqY, 20);
         var numero_pulso_esq=Number(pulso_esqY);
@@ -66,5 +87,5 @@ function draw(){
         var volume=remove_decimais/500;
         document.getElementById("volume").innerHTML="volume: "+volume;
         musica.setVolume(volume);
-    //}
+    }
 }
